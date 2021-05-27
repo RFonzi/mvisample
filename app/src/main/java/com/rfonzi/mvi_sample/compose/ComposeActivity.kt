afba2c9a5a983330f1e1f63a421d15008b6fd499
Ryan Fonzi <ryan.fonzi@gmail.com>
2021-05-27 16:59:22 -0400

    Add compose

---
+++ app/src/main/java/com/rfonzi/mvi_sample/compose/ComposeActivity.kt
@@ -0,0 +1,38 @@
+package com.rfonzi.mvi_sample.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.rfonzi.mvi_sample.compose.ui.theme.MvisampleTheme
+
+class ComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MvisampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MvisampleTheme {
+        Greeting("Android")
+    }
+}
